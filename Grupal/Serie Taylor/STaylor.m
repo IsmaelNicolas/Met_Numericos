f3fa301c21961fc6eb459a83e46dc69cb469d1a3
Ismael Nicolas <ismael.cedillo@outlook.es>
2021-11-21 17:18:16 -0500

    fx is validated

--- Grupal/Serie Taylor/STaylor.m
+++ Grupal/Serie Taylor/STaylor.m
@@ -1,4 +1,5 @@
 function [tx,Rx,R,r,Ea] = STaylor(fx,c,e,N,x)
+    clc
     [v_fx,v_c,v_e,v_N,v_x] = validate(lower(fx),lower(c),lower(e),lower(N),lower(x));
     v = [v_fx,v_c,v_e,v_N,v_x];
     if all(v==true)
@@ -21,19 +22,19 @@ end
 
 function [v_fx,v_c,v_e,v_N,v_x] = validate(fx,c,e,N,x)
     
-    trasedental = ['sin','cos','tan','exp','csc','log']; 
+    tras = ["sin",'cos','tan','exp','csc','log','sinh','cosh','tanh',]; 
     
-    if(fx == 'sin')
-        v_fx = true;
-        v_c = true;
-        v_e = true;
-        v_N = true;
-        v_x = true;
-    else 
-        v_fx = false;
-        v_c = false;
-        v_e = false;
-        v_N = false;
-        v_x = false;
+    v_fx = false;
+    v_c = false;
+    v_e = false;
+    v_N = false;
+    v_x = false;
+    
+    for i = 1:length(tras)
+        if fx==tras(1,i)
+            v_fx = true;
+            return;
+        end
     end
+        
 end
