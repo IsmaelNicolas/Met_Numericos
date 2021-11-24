8a05f15bec840f621dbea70cf8bc9224099d66de
Ismael Nicolas <ismael.cedillo@outlook.es>
2021-11-23 20:45:12 -0500

    Update Taylor2

--- Grupal/Serie Taylor/STaylor.m
+++ Grupal/Serie Taylor/STaylor.m
@@ -113,9 +113,12 @@ tic
         case 2
             fx = varargin{1};
             n = varargin{2};
-            [a,b] = taylor1(fx,n);
-            varargout{1} = a;
-            varargout{2} = b;
+            [varargout{1},varargout{2}] = taylor1(fx,n);
+        case 3
+            fx = varargin{1};
+            n = varargin{2};
+            c = varargin{3};
+            [varargout{1},varargout{2},varargout{3},varargout{4}] = taylor2(fx,n,c);           
         otherwise
             error('Ingrese los parametros correctos')
     end
@@ -189,6 +192,17 @@ function [v_fx,v_n,v_c,v_e,v_N,v_x] = validate(fx,n,c,e,N,x)
    
 end
 
+function print(varargin)
+    
+    var = {'f(x): ','n: ','t(x): ','Rt(x): ','c','R','r','e','N','Ea(x)','Er(x)'};
+    fprintf('<strong>\t Impresion de datos.\n</strong>')
+    for i = 1:nargin
+        fprintf('<strong>%s</strong>',var{i})
+        disp(varargin{i})
+    end
+        
+end
+
 function [tx,Rtx] = taylor1(fx,n)
     syms c x e
     fxs=str2sym(fx);%convertir la funcion a simbolica
@@ -198,14 +212,32 @@ function [tx,Rtx] = taylor1(fx,n)
         tx(i+1)= poly2sym(fliplr(s(1:i+1)),vs);
     end
     Rtx = (subs(diff(fxs,n),vs,e)*x^(n+1))/factorial(n+1);
-     
-    %Imprimo los valores 
-     disp('<strong>Impresion de datos.</strong>')
-     fprintf('<strong>f(x):</strong> %s\n',fxs)
-     fprintf('<strong>n:</strong> %d\n',n)
-     fprintf('<strong>t(x):</strong> %s\n',tx(end))
-     fprintf('<strong>Rt(x):</strong> %s\n',Rtx)
+    fplot(fxs,'LineWidth',2); grid on;
     
+    %fplot(tx,'LineWidth',2);
+    %Imprimo los valores
+    print(fxs,n,tx(end),Rtx)
     
+end
+
+function [tx,Rtx,R,r] = taylor2(fx,n,c)
+
+    syms  x e
+    fxs=str2sym(fx);%convertir la funcion a simbolica
+    vs = symvar(fxs);%Encontrar la variable simbólica en fxs.
+    for i=0:n
+        s(i+1)=subs(diff(fxs,vs,i),vs,c)/factorial(i)*(x-c)^i;
+        tx(i+1)= poly2sym(fliplr(s(1:i+1)),vs);
+    end
+    Rtx = (subs(diff(fxs,n),vs,e)*x^(n+1))/factorial(n+1);
+    R=0;r=0;
+    for i = 2:n+1
+       hold on
+       fplot(tx(i));
+    end
+    plot(c,subs(fxs,vs,c),'ro','MarkerFaceColor','r');
+    %Imprimo los valores
+    print(fxs,n,tx(end),Rtx,c,R,r)
+
 end
 
