c82b67843253c68ff790b918d5b00b31e5dee592
Ismael Nicolas <ismael.cedillo@outlook.es>
2022-01-08 15:58:03 -0500

    Clean interpolacion de newton

--- Grupal/Interpolacion de Newton/fnwt.m
+++ Grupal/Interpolacion de Newton/fnwt.m
@@ -16,31 +16,40 @@ function varargout = fnwt(varargin)
 % En caso del [Pn,Rt] = FNWT(A) se devolvera de 
 % manera  algebraica y en el [Pn,Rt] = FNWT(A,x)
 % como valor numerico
+tic
+A = varargin{1};
 
+if nargout == 0
+    fprintf(2,'<strong>No tiene parametros de salida\n</strong>');
+end
 
 switch nargin
     
     case 1
        [varargout{1},varargout{2}] = newton(A);
     case 2
+        x = varargin{2};
         [varargout{1},varargout{2}] = evaluar(A,x);
     otherwise
         fprintf(2,'<strong>Ingresa un Ajuste valido\n</strong>');
         
 end
 
+
     function [Pn,Rt] = newton(A)
         A = A';
         disp(A)
+        Pn = 0;
+        Rt = 1;
     end
 
-    function evaluar(A,x)
+    function [Pn,Rt] = evaluar(A,x)
         [Pn,Rt] = newton(A);
         disp(x)
         disp(Pn)
         disp(Rt)
     end
     
-
+toc
 end
 
